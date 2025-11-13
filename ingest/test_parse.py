595529c935fe0dc36b22820757565c18f3fd45c5
Harmeet Kaur <meet.academia@gmail.com>
2025-11-13 12:09:18 -0500

    parse the fetched data and save as JSON file

--- ingest/test_parse.py
+++ ingest/test_parse.py
@@ -2,8 +2,16 @@ from fetch_trials import fetch_trials
 from parse_trials import parse_trials
 import json
 
+#Fetch and parse
 raw_trials = fetch_trials()
 parsed = parse_trials(raw_trials)
 
-print(f"Parsed {len(parsed)} trials.")
-print(json.dumps(parsed[0], indent=2))
+#Uncomment to investigate the output
+# print(f"Parsed {len(parsed)} trials.")
+# print(json.dumps(parsed[0], indent=2))
+
+# Save to JSON
+with open("../data/parsed_trials.json", "w", encoding="utf-8") as f:
+    json.dump(parsed, f, indent=2, ensure_ascii=False)
+
+print(f"Saved {len(parsed)} trials to data/parsed_trials.json")
