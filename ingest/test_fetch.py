3a4dd2f6f2ec83caf8556ddc4afaccb8a41d213b
Harmeet Kaur <meet.academia@gmail.com>
2025-11-13 10:44:12 -0500

    Test code to investigate the fetched output from api call

--- ingest/test_fetch.py
+++ ingest/test_fetch.py
@@ -1,14 +1,23 @@
 import json
-from .fetch_trials import fetch_trials
+from fetch_trials import fetch_trials
 
 # Fetch trials updated in the last 12 months
-data = fetch_trials(condition="Pediatric Brain Tumor", page_size=5, months_back=12)
-
-# Print summary
-print(f"\n Retrieved {len(data.get('studies', []))} trials.\n")
-
-# Optional: Pretty-print the first trial
-if data.get("studies"):
-    print(json.dumps(data["studies"][0], indent=2))
+data = fetch_trials()
+# print(data.keys())  # Top-level keys (output: dict_keys(['studies', 'nextPageToken']))
+#Check total number of records fetched
+print("Number of trials fetched:", len(data))
+# Preview first trial
+if data:
+    print(json.dumps(data[0], indent=2))
 else:
-    print("No trials found.")
+    print("No trials returned.")
+
+
+# # Print summary
+# print(f"\n Retrieved {len(data.get('studies', []))} trials.\n")
+
+# # Optional: Pretty-print the first trial
+# if data.get("studies"):
+#     print(json.dumps(data["studies"][0], indent=2))
+# else:
+#     print("No trials found.")
